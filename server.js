078af7ca8307849f5ce364f8cb2c0b15edddd785
Violet Kim <violetshirokuma@gmail.com>
2020-05-13 17:13:31 -0700

    Adding and fixing all files

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+const PORT = process.env.PORT || 3008;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routes/html_routes")(app);
+require("./routes/api_routes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
